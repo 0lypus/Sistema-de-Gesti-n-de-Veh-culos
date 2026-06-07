72040fd16aec7f7bc1e6b431a6b8d3dc636db4d6
Charlie <cmartinheredial22@gmail.com>
2026-06-07 20:59:25 -0300

    se crearon las listas de los tipos de vehiculos y se prepara el menu

--- codigo/Program.cs
+++ codigo/Program.cs
@@ -1,1 +1,39 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static List<Moto> motos = new List<Moto>();
+    static List<Auto> autos = new List<Auto>();
+    static void Main()
+    {
+        Console.WriteLine("Bienvenido a la aplicación de vehículos");
+        while (true)
+        {
+            Console.WriteLine("Seleccione una opción:");
+            Console.WriteLine("1. Crear Moto");
+            Console.WriteLine("2. Crear Auto");
+            Console.WriteLine("3. Listar Vehículos");
+            Console.WriteLine("4. Salir");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    RegistrarVehiculo();
+                    break;
+                case "2":
+                    Simulacion();
+                    break;
+                case "3":
+                    MostrarInformacion();
+                    break;
+                case "4":
+                    Console.WriteLine("Cerrando programa..");
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida, intente nuevamente.");
+                    break;
+            }
+        }
+    }
+}
