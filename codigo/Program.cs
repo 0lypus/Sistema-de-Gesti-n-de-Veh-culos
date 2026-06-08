f24251f5b08b5bee49e8f8ff21c25901fca48740
Charlie <cmartinheredial22@gmail.com>
2026-06-07 21:05:33 -0300

    se creo la funcion de registro

--- codigo/Program.cs
+++ codigo/Program.cs
@@ -9,9 +9,9 @@ class Program
         while (true)
         {
             Console.WriteLine("Seleccione una opción:");
-            Console.WriteLine("1. Crear Moto");
-            Console.WriteLine("2. Crear Auto");
-            Console.WriteLine("3. Listar Vehículos");
+            Console.WriteLine("1. Registrar vehiculo");
+            Console.WriteLine("2. Simulación de Aceleración y Frenado");
+            Console.WriteLine("3. Mostrar información");
             Console.WriteLine("4. Salir");
 
             string opcion = Console.ReadLine();
@@ -33,6 +33,33 @@ class Program
                 default:
                     Console.WriteLine("Opción no válida, intente nuevamente.");
                     break;
+            }
+           static void RegistrarVehiculo()
+            {
+                Console.WriteLine("Seleccione el tipo de vehículo a registrar: (moto/auto)");
+
+                string tipo = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la marca del vehículo:");
+                string marca = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el modelo del vehículo:");
+                string modelo = Console.ReadLine();
+
+               else  if (tipo.ToLower()== "moto")
+                {
+                    motos.Add(new Moto(marca, modelo));
+                    Console.WriteLine("Moto registrada exitosamente.");
+                }
+                else if (tipo.ToLower()== "auto")
+                {
+                    autos.Add(new Auto(marca, modelo));
+                    Console.WriteLine("Auto registrado exitosamente.");
+                }
+                else
+                {
+                    Console.WriteLine("Tipo de vehículo no válido.");
+                }
             }
         }
     }
